ffb0e6a8e794b79a59b05d489d55220f989ff9f1
akiragovender <govender.akira@gmail.com>
2018-04-12 13:13:06 +0200

    Edit the meta-data and file descriptions

--- Day_1.R
+++ Day_1.R
@@ -1,8 +1,6 @@
-# Akira Govender 
-# 12 April 2018
+# Akira Govender
 # Day_1 
+# The first day of the stats class
+# Purpose: To practise some concepts that we will encounter 
+# 12 April 2018
 
-
-# Setup  ------------------------------------------------------------------
-
-# I love food 
